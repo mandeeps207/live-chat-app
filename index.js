26851ca09726846a77ca0fb7f5adc87d127b0e53
TechClouds207 <139922172+TechClouds207@users.noreply.github.com>
2024-07-02 19:45:51 +0530

    session setup done with register and login options

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import path from 'path';
 import sessionMiddleware from './models/session.js';
 import bodyParser from 'body-parser';
 import 'dotenv/config';
+import db from './models/database.js';
+import bcrypt from 'bcryptjs/dist/bcrypt.js';
 
 const app = express();
 const __dirname = path.resolve();
@@ -21,16 +23,67 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.render('home', {user: req.session.user});
 });
-app.get('/register', (req, res) => {
-    res.send('Future register page!');
-});
 app.get('/login', (req, res) => {
-    req.session.user = {username: "Mandeep Singh"};
-    res.redirect('/');
+    res.render('login');
 });
 app.get('/logout', (req, res) => {
     req.session.destroy();
     res.redirect('/');
+});
+
+// API Routes
+app.post('/login', (req, res) => {
+    const {username, password, formOption} = req.body;
+    if (formOption === 'register') {
+        db.get('SELECT username FROM users WHERE username = ?', [username], (err, row) => {
+            if (row) {
+                res.send({status: 409, message: 'Username already exists. Please choose a different one or try login option.'})
+            } else {
+                const salt = bcrypt.genSaltSync(10);
+                const hashedPass = bcrypt.hashSync(password, salt);
+                db.run('INSERT INTO users (username, password) VALUES (?, ?)', [username, hashedPass], (err) => {
+                    if(err) {
+                        res.send({status: 503, message: 'Error in creating account.'});
+                    } else {
+                        req.session.user = { username };
+                        res.send({status: 200, message: 'Account created and logging to chatroom.'});
+                    }
+                })
+            }
+        });
+    }
+    if (formOption === 'login') {
+        db.get('SELECT * FROM users WHERE username = ?', [username], (err, row) => {
+            if (!row) {
+                res.send({message: 'Invalid username or password', status: 403});
+            } else {
+                const isValid = bcrypt.compareSync(password, row.password);
+                if (isValid) {
+                    req.session.user = { username: row.username };
+                    res.send({message: 'Login suceessfully!', status: 200})
+                } else {
+                    res.send({message: 'Invalid username or password', status: 403});
+                }
+            }
+        });
+    }
+
+    // db.get('SELECT username FROM users WHERE username = ?', [username], (err, row) => {
+    //     if (row) {
+    //         res.send({status: 409, message: 'Username already exists. Please choose a different one.'})
+    //     } else {
+    //         const salt = bcrypt.genSaltSync(10);
+    //         const hashedPass = bcrypt.hashSync(password, salt);
+    //         db.run('INSERT INTO users (username, password) VALUES (?, ?)', [username, hashedPass], (err) => {
+    //             if(err) {
+    //                 res.send({status: 503, message: 'Error in creating account.'});
+    //             } else {
+    //                 req.session.user = { username };
+    //                 res.send({status: 200, message: 'Account created and logging to chatroom.'});
+    //             }
+    //         })
+    //     }
+    // });
 });
 
 // Initiating server
---
+++ public/login.js
@@ -0,0 +1,38 @@
+// Login form handler
+const form = document.querySelector('#login-form');
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const username = form.elements['username'].value;
+    const password = form.elements['password'].value;
+    const result = document.querySelector('#error');
+    const formOption = form.elements['loginOption'].value;
+    console.log(username, password, formOption);
+    let options = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({username, password, formOption})
+    };
+    try {
+        const res = await fetch('/login', options);
+        const {status, message} = await res.json();
+        if(status === 409) {
+            result.classList.add('text-danger');
+            result.innerHTML = message;
+        }
+        if(status === 403) {
+            result.classList.add('text-danger');
+            result.innerHTML = message;
+        }
+        if(status === 200) {
+            result.classList.remove('text-danger');
+            result.classList.add('text-success');
+            result.innerHTML = message;
+            setTimeout(() => {
+                window.location.href = ('/');
+            }, 2000);
+        }
+    } catch (error) {
+        result.classList.add('text-danger');
+        result.innerHTML = error.message;
+    }
+});
