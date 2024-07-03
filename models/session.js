ea7737abc67d86652c421c5558401303169a7e53
TechClouds207 <139922172+TechClouds207@users.noreply.github.com>
2024-07-03 15:17:33 +0530

    pg-connect-session setup for better session manage on vercel

--- models/session.js
+++ models/session.js
@@ -1,12 +1,24 @@
 import session from 'express-session';
 import 'dotenv/config';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+    connectionString: process.env.LIVE_CHAT_APP_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 const sessionOptions = {
+    store: new (require('connect-pg-simple')(session)({
+        pool, // Connection pool
+        tableName: 'user_sessions' // Use a different table name if desired
+    })),
     secret: process.env.SESSION_KEY,
     resave: false,
     saveUninitialized: true,
     cookie: { 
-        secure: true, // Set true for HTTPS
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 Days
     }
 };
 
